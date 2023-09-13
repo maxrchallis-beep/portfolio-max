1500f80cdfd3bacfb12afbb11bd23eb436bfe4a5
Glitch (glitch-hello-website) <none>
2023-09-13 02:56:40 +0000

    🌆🐆 Checkpoint ./script.js:47950344/2389 ./style.css:47950344/674 ./gd-athome.html:47950344/112

--- script.js
+++ script.js
@@ -8,21 +8,3 @@
             document.getElementById("view2").style.display = "block";
         }
 
-
-function showUnderline1() {
-  var element = document.getElementById("view1")
-  
-  element.style.textDecoration = "underline";
-  }
-
-const myButton1 = document.getElementById("myButton1");
-const myButton2 = document.getElementById("myButton2");
-
-// Function to handle button click
-function handleButtonClick() {
-    // Toggle the "selected" class on the button
-    myButton1.classList.toggle("selected");
-}
-
-// Add click event listener to the button
-myButton1.addEventListener("click", handleButtonClick);

b712fa2a83f83abcc58bb30b4663db8dc6443dcb
Glitch (glitch-hello-website) <none>
2023-09-13 02:33:52 +0000

    🗽🌞 Checkpoint ./gd-athome.html:47950344/162 ./style.css:47950344/20 ./script.js:47950344/2070

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
         function showView1() {
             document.getElementById("view1").style.display = "block";
             document.getElementById("view2").style.display = "none";
-          this.classList.add('underlined');
         }
 
         function showView2() {
             document.getElementById("view1").style.display = "none";
             document.getElementById("view2").style.display = "block";
         }
+
+
+function showUnderline1() {
+  var element = document.getElementById("view1")
+  
+  element.style.textDecoration = "underline";
+  }
